8dc0fdb9827de4fae2d15872cb036051f11587c9
Umair Idris <umairidris@google.com>
2019-05-06 15:42:12 -0700

    set custom boot image on CFT resource if defined
    PiperOrigin-RevId: 246911430

--- deploy/cft/gce_instance.go
+++ deploy/cft/gce_instance.go
@@ -1,18 +1,30 @@
 package cft
 
+import "errors"
+
 // GCEInstance wraps a CFT GCE Instance.
 type GCEInstance struct {
 	GCEInstanceProperties `json:"properties"`
+	CustomBootImage       *struct {
+		ImageName string `json:"image_name"`
+	} `json:"custom_boot_image,omitempty"`
 }
 
 // GCEInstanceProperties represents a partial CFT instance implementation.
 type GCEInstanceProperties struct {
 	GCEInstanceName string `json:"name"`
 	Zone            string `json:"zone"`
+	DiskImage       string `json:"diskImage,omitempty"`
 }
 
 // Init initializes the instance.
 func (i *GCEInstance) Init(*Project) error {
+	if i.CustomBootImage != nil {
+		if i.DiskImage != "" {
+			return errors.New("custom boot image and disk image cannot both be set")
+		}
+		i.DiskImage = "global/images/" + i.CustomBootImage.ImageName
+	}
 	return nil
 }
 
--- deploy/cft/gce_instance_test.go
+++ deploy/cft/gce_instance_test.go
@@ -22,7 +22,7 @@ properties:
 	}
 
 	if err := ins.Init(project); err != nil {
-		t.Fatalf("d.Init: %v", err)
+		t.Fatalf("ins.Init: %v", err)
 	}
 
 	got := make(map[string]interface{})
@@ -44,5 +44,31 @@ properties:
 
 	if gotName, wantName := ins.Name(), "foo-instance"; gotName != wantName {
 		t.Errorf("ins.Name() = %v, want %v", gotName, wantName)
+	}
+}
+
+func TestGCEInstance_CustomBootImage(t *testing.T) {
+	_, project := getTestConfigAndProject(t, nil)
+
+	instanceYAML := `
+properties:
+  name: foo-instance
+  zone: us-east1-a
+custom_boot_image:
+  image_name: foo-OS
+  gcs_path: foo-os/some-image.tar.gz
+`
+
+	ins := &GCEInstance{}
+	if err := yaml.Unmarshal([]byte(instanceYAML), ins); err != nil {
+		t.Fatalf("yaml unmarshal: %v", err)
+	}
+
+	if err := ins.Init(project); err != nil {
+		t.Fatalf("ins.Init: %v", err)
+	}
+
+	if got, want := ins.DiskImage, "global/images/foo-OS"; got != want {
+		t.Errorf("disk image custom boot image: got %q, want %q", got, want)
 	}
 }
