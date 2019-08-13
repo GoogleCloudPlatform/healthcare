c17c619fbfc6b9f512c2d1653bf79e65704e7f79
xingao <xingao@google.com>
2019-08-13 12:28:09 -0700

    Fix empty allowed_apis list forbids any apis to enable.
    PiperOrigin-RevId: 263190791

--- deploy/config/config.go
+++ deploy/config/config.go
@@ -127,6 +127,9 @@ func (c *Config) Init(genFields *AllGeneratedFields) error {
 
 // validate validates the config.
 func (c *Config) validate() error {
+	if len(c.Overall.AllowedAPIs) == 0 {
+		return nil
+	}
 	// Enforce allowed_apis in overall project config.
 	allowedAPIs := make(map[string]bool)
 	for _, a := range c.Overall.AllowedAPIs {
