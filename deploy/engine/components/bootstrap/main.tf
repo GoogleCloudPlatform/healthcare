f49b793ff0495ea4e167c0c50913bd983e2fbd80
Xin Gao <xingao@google.com>
2020-04-17 17:47:11 -0700

    update boostrap project template
    PiperOrigin-RevId: 307146419

--- deploy/engine/components/bootstrap/main.tf
+++ deploy/engine/components/bootstrap/main.tf
@@ -1,26 +1,54 @@
-// TODO: replace with https://github.com/terraform-google-modules/terraform-google-bootstrap
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# This folder contains Terraform resources to setup the devops project, which includes:
+# - The project itself,
+# - APIs to enable,
+# - Deletion lien,
+# - Project level IAM permissions for the project owners,
+# - A Cloud Storage bucket to store Terraform states for all deployments,
+# - Org level IAM permissions for org admins.
+
+// TODO: replace with https://github.com/terraform-google-modules/terraform-google-bootstrap
 {{if get . "ENABLE_BOOTSTRAP_GCS_BACKEND"}}
+# ==============================================================================
+# TODO: Uncomment after initial deployment and run `terraform init`.
 terraform {
   backend "gcs" {
     bucket = "{{.STATE_BUCKET}}"
     prefix = "bootstrap"
   }
 }
+# ==============================================================================
 {{end}}
-
+# Devops project, with APIs to enable and deletion lien created.
 module "project" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 7.0"
 
-  name                    = var.project_id
+  name                    = var.devops_project_id
   org_id                  = var.org_id
   billing_account         = var.billing_account
   lien                    = true
   default_service_account = "keep"
   skip_gcloud_download    = true
+  activate_apis = [
+    "cloudbuild.googleapis.com",
+  ]
 }
 
+# Terraform state bucket, hosted in the devops project.
 module "state_bucket" {
   source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version = "~> 1.4"
@@ -28,4 +56,18 @@ module "state_bucket" {
   name       = var.state_bucket
   project_id = module.project.project_id
   location   = var.storage_location
+}
+
+# Project level IAM permissions for devops project owners.
+resource "google_project_iam_binding" "devops_owners" {
+  project = module.project.project_id
+  role    = "roles/owner"
+  members = var.devops_owners
+}
+
+# Org level IAM permissions for org admins.
+resource "google_organization_iam_member" "org_admin" {
+  org_id = var.org_id
+  role   = "roles/resourcemanager.organizationAdmin"
+  member = var.org_admin
 }
--- deploy/engine/components/bootstrap/variables.tf
+++ deploy/engine/components/bootstrap/variables.tf
@@ -1,5 +1,23 @@
-variable "project_id" {
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "devops_project_id" {
   type = string
+}
+
+variable "devops_owners" {
+  type = list(string)
 }
 
 variable "org_id" {
@@ -15,5 +33,9 @@ variable "state_bucket" {
 }
 
 variable "storage_location" {
+  type = string
+}
+
+variable "org_admin" {
   type = string
 }
