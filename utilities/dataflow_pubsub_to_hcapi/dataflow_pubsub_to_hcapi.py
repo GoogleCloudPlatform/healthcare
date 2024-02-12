4a64d27de982b2728c9ea59deea5b5190577410d
Devansh Modi <devanshmodi@google.com>
2024-02-12 15:25:59 -0600

    updated license and resolved review comments

--- utilities/dataflow_pubsub_to_hcapi/dataflow_pubsub_to_hcapi.py
+++ utilities/dataflow_pubsub_to_hcapi/dataflow_pubsub_to_hcapi.py
@@ -1,18 +1,16 @@
-# @title ###### Licensed to the Apache Software Foundation (ASF), Version 2.0 (the "License")
+#    Copyright 2023 Google LLC
 
-# Copyright 2023 Google LLC
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
 
 #        http://www.apache.org/licenses/LICENSE-2.0
 
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 
 ##################################
@@ -197,6 +195,7 @@ class hcapi_fhir_store:
         postResponse,responseFlag = self.hcapi_post(url,payload)
         return postResponse,responseFlag
 
+    #Below get_fhir_message function only kept for future reference not used in the script
     def get_fhir_message(self, url_path:str):
         """ Function to fetch FHIR resources from HCAPI"""
         """
